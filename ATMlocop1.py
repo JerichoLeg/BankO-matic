67b8b244ba9bcc6ca19aceea22f88e3468808680
kcmateo <61491274+kcmateo@users.noreply.github.com>
2020-04-21 18:03:03 +0800

    Add files via upload

---
+++ ATMlocop1.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ATMlocop1.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(540, 360)
+        MainWindow.setMinimumSize(QtCore.QSize(540, 360))
+        MainWindow.setMaximumSize(QtCore.QSize(540, 360))
+        MainWindow.setStyleSheet("*{\n"
+"font-family: Akrobat;\n"
+"}\n"
+"QMainWindow{\n"
+"border-image:url(:/images/BG.jpg)\n"
+"}\n"
+"QLabel{\n"
+"font-family: Akrobat;\n"
+"font-size: 22px;\n"
+"}\n"
+"QToolButton{\n"
+"background: transparent\n"
+"}\n"
+"QToolButton:hover{\n"
+"background: #E0ECF8;\n"
+"border-radius: 7px;\n"
+"}\n"
+"QPushButton{\n"
+"color:#dfdfdf;\n"
+"background:#00264d;\n"
+"border-radius: 10px;\n"
+"}\n"
+"QLineEdit{\n"
+"border-radius:10px;\n"
+"background: #dfdfdf;\n"
+"font-size:15px;\n"
+"}\n"
+"QLineEdit:hover{\n"
+"border-radius:10px;\n"
+"background: #ffffff;\n"
+"}\n"
+"Qlabel{\n"
+"color:#dfdfdf;\n"
+"background:transparent;\n"
+"}")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.toolButton = QtWidgets.QToolButton(self.centralwidget)
+        self.toolButton.setGeometry(QtCore.QRect(500, 10, 31, 31))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/images/icons8-close-window-96.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.toolButton.setIcon(icon)
+        self.toolButton.setIconSize(QtCore.QSize(50, 50))
+        self.toolButton.setObjectName("toolButton")
+        self.label_logo = QtWidgets.QLabel(self.centralwidget)
+        self.label_logo.setGeometry(QtCore.QRect(180, -10, 181, 121))
+        self.label_logo.setStyleSheet("image: url(:/images/Logo.png)")
+        self.label_logo.setText("")
+        self.label_logo.setObjectName("label_logo")
+        self.commandLinkButton = QtWidgets.QCommandLinkButton(self.centralwidget)
+        self.commandLinkButton.setGeometry(QtCore.QRect(0, 0, 181, 41))
+        self.commandLinkButton.setMouseTracking(True)
+        self.commandLinkButton.setObjectName("commandLinkButton")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(200, 70, 141, 41))
+        self.label.setObjectName("label")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(70, 110, 411, 211))
+        self.textBrowser.setObjectName("textBrowser")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(270, 330, 121, 41))
+        self.label_2.setObjectName("label_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.toolButton.clicked.connect(MainWindow.close)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.toolButton.setText(_translate("MainWindow", "..."))
+        self.commandLinkButton.setText(_translate("MainWindow", "Back to Main Menu"))
+        self.label.setText(_translate("MainWindow", "ATM Locator"))
+        self.label_2.setText(_translate("MainWindow", "ATM Locator"))
+import res_rc
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
