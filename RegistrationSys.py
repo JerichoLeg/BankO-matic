fff90c566284ed93254ff65f2334e51673845ea2
John Alfred Gascon <52269243+Javgascon@users.noreply.github.com>
2020-04-25 05:52:18 +0800

    Update RegistrationSys.py

--- RegistrationSys.py
+++ RegistrationSys.py
@@ -1,24 +1,28 @@
+#*
+
+#*
 import sqlite3
 import login
 
-
 with sqlite3.connect("Registration.db") as db:
     cursor = db.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS user(userID INTEGER PRIMARY KEY, username VARCHAR(20) NOT NULL,
-Name VARCHAR(20) NOT NULL, Password VARCHAR(20) NOT NULL);''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS user(userID INTEGER PRIMARY KEY, FName VARCHAR(20) NOT NULL,
+LName VARCHAR(20) NOT NULL,UserName VARCHAR(20) NOt NULL, Password VARCHAR(20) NOT NULL);''')
 
-cursor.execute("""INSERT INTO user(username,Name,Password)VALUES("test_User","John Gascon","password")""")
+cursor.execute("""INSERT INTO user(FName,LName,UserName, Password)VALUES("John","Gascon","User_Testing","password")""")
 db.commit()
 
 
 def newUser():
     found = 0
     while found == 0:
-        username = input("Please Enter a username: ")
+        FName = input("First Name: ")
+        LName = input("Last Name: ")
+        UserName = input("Please Enter a username: ")
         with sqlite3.connect("Registration.db") as db:
             cursor = db.cursor()
-            finduser = ("SELECT * FROM user WHERE username = ?")
-            cursor.execute(finduser,[(username)])
+            finduser = "SELECT * FROM user WHERE UserName = ?"
+            cursor.execute(finduser,[(UserName)])
         if cursor.fetchall():
             print("That username already Exist! Please Try Again")
         else:
@@ -32,8 +36,8 @@ def newUser():
             print("Sorry your password does not match at all. Try Again")
             Password = input("Enter Password: ")
             Password1 = input("Enter Password Again: ")
-        insertData = '''INSERT INTO user(username, Name, Password) VALUES(?,?,?)'''
-        cursor.execute(insertData, [username, Name, Password])
+        insertData = '''INSERT INTO user(FName, LName, UserName, Password) VALUES(?,?,?)'''
+        cursor.execute(insertData, [FName, LName, UserName, Password])
         db.commit()
 
 
@@ -41,3 +45,4 @@ def newUser():
 newUser()
 cursor.execute("SELECT *  FROM user;")
 print(cursor.fetchall())
+
